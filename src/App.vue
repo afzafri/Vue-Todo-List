0e40a4b315b45c0e627686e416664d27dcc1ed15
Afif Zafri <afif@redant.my>
2019-11-12 16:16:29 +0800

    iterate object and display

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,36 @@
 <template>
   <div id="app">
-
+    <Todos v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos';
 
 export default {
   name: 'app',
   components: {
-
+    Todos
   },
   data() {
     return {
-      
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        },
+      ]
     }
   }
 }
