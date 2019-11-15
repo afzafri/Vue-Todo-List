703c824a5d5be85080f6ee1f2bd6e1556945262b
Afif Zafri <afif@redant.my>
2019-11-15 09:17:13 +0800

    .

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,14 +2,14 @@ import * as firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const config = {
-  apiKey: "xxxxx",
-  authDomain: "xxxxx",
-  databaseURL: "xxxxx",
-  projectId: "xxxxx",
-  storageBucket: "xxxxx",
-  messagingSenderId: "xxxxx",
-  appId: "xxxxx",
-  measurementId: "xxxxx"
+  apiKey: "AIzaSyC_Fre6S-p0ou4joW94n9WzGhC68MK3sSw",
+  authDomain: "todolist-f9c2b.firebaseapp.com",
+  databaseURL: "https://todolist-f9c2b.firebaseio.com",
+  projectId: "todolist-f9c2b",
+  storageBucket: "todolist-f9c2b.appspot.com",
+  messagingSenderId: "603454711668",
+  appId: "1:603454711668:web:c559a901cef96347902e21",
+  measurementId: "G-LEB4M7XVK3"
 };
 // Initialize Firebase
 firebase.initializeApp(config);
